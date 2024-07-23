022d009856cb476a4c51c6f43ca8a90d16bd7636
sridamul <sridamul@gmail.com>
2024-07-23 16:02:29 +0530

    Update Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+
+const getYear = () => {
+  return new Date().getFullYear();
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const bannerCondensed = 
+    " ________  _______    __     ________       __       ___      ___  ____  ____  ___       \n" +
+    " /\"       )/\"      \\  |\" \\   |\"      \"\\     /\"\"\\     |\"  \\    /\"  |(\"  _||_ \" ||\"  |      \n" +
+    "(:   \\___/|:        | ||  |  (.  ___  :)   /    \\     \\   \\  //   ||   (  ) : |||  |      \n" +
+    " \\___  \\  |_____/   ) |:  |  |: \\   ) ||  /' /\\  \\    /\\\\  \\/.    |(:  |  | . )|:  |      \n" +
+    "  __/  \\\\  //      /  |.  |  (| (___\\ || //  __'  \\  |: \\.        | \\\\ \\__/ //  \\  |___   \n" +
+    " /\" \\   :)|:  __   \\  /\\  |\\ |:       :)/   /  \\\\  \\ |.  \\    /:  | /\\\\ __ //\\ ( \\_|:  \\  \n" +
+    "(_______/ |__|  \\___)(__\\_|_)(________/(___/    \\___)|___|\\__/|___|(__________) \\_______) \n" +
+    "  \u00A9 " + getYear();
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <pre>{bannerCondensed}</pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
