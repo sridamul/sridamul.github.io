2553908c7b807e6ffab534b4cfde0808ad5dd0a9
sridamul <sridamul@gmail.com>
2024-07-25 00:45:18 +0530

    Add autocomplete for github command

--- src/utils/autocomplete.ts
+++ src/utils/autocomplete.ts
@@ -4,7 +4,7 @@ export const getAutocompleteSuggestions = (
   input: string,
   currentDirectory: FileSystemItem[]
 ): string[] => {
-  const commands = ['compgen', 'help', 'clear', 'ls', 'cd', 'cat', 'man', 'history', 'date'];
+  const commands = ['compgen', 'help', 'clear', 'ls', 'cd', 'cat', 'man', 'history', 'date', 'github'];
   const suggestions = commands.filter((command) => command.startsWith(input));
 
   const currentDirectoryItems = currentDirectory
