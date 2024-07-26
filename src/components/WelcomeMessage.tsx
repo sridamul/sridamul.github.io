c633b2a603ab9ae9e9bc35a1be06a049a8cda655
sridamul <sridamul@gmail.com>
2024-07-27 01:59:21 +0530

    Revert "Add typing audio to welcome message"
    This reverts commit 74365ad3758b45da4af4bda0a97d17cbb87427bc.

--- src/components/WelcomeMessage.tsx
+++ src/components/WelcomeMessage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
 // Credits: Craig feldmann
 // GitHub Link: https://github.com/craig-feldman
@@ -9,55 +9,32 @@ type WelcomeMessageProps = {
 };
 
 const WelcomeMessage: React.FC<WelcomeMessageProps> = (props) => {
-  const welcomeMessageRef = useRef<HTMLDivElement>(null);
-  const typingSoundRef = useRef<HTMLAudioElement>(new Audio('/keyboard-typing.mp3'));
+  const welcomeMessageRef = React.useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const typingSound = typingSoundRef.current;
+    if (props.inputRef.current) {
+      props.inputRef.current.disabled = true;
+    }
 
-    typingSound.preload = 'auto';
-
-    const onCanPlayThrough = () => {
-      if (props.inputRef.current) {
-        props.inputRef.current.disabled = true;
+    let index = 0;
+    const typeText = setInterval(() => {
+      if (!welcomeMessageRef.current) {
+        return;
       }
 
-      typingSound.pause();
-      typingSound.currentTime = 0;
+      welcomeMessageRef.current.insertAdjacentText("beforeend", props.message.charAt(index));
+      index++;
 
-      let index = 0;
-      const typeText = setInterval(() => {
-        if (!welcomeMessageRef.current) {
-          return;
-        }
-
-        welcomeMessageRef.current.insertAdjacentText("beforeend", props.message.charAt(index));
-        typingSound.play();
-        index++;
-
-        if (index === props.message.length) {
-          clearInterval(typeText);
-          if (props.inputRef.current) {
-            props.inputRef.current.disabled = false;
-            props.inputRef.current.focus();
-          }
-          typingSound.pause();
-          typingSound.currentTime = 0;
-        }
-      }, 30);
-
-      return () => {
+      if (index === props.message.length) {
         clearInterval(typeText);
-        typingSound.pause();
-        typingSound.currentTime = 0;
-      };
-    };
+        if (props.inputRef.current) {
+          props.inputRef.current.disabled = false;
+          props.inputRef.current.focus();
+        }
+      }
+    }, 30);
 
-    typingSound.addEventListener('canplaythrough', onCanPlayThrough, { once: true });
-
-    return () => {
-      typingSound.removeEventListener('canplaythrough', onCanPlayThrough);
-    };
+    return () => clearInterval(typeText);
   }, [props.inputRef, props.message]);
 
   return <div ref={welcomeMessageRef} className="terminal-welcome-message"></div>;
